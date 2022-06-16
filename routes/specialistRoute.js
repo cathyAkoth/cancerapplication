132ef532401a2222eb772cc14eabccb0caf31f2f
Cathy Akoth <cathyakoth@Cathys-MacBook-Air.local>
2022-06-16 22:26:36 +0300

    Work on specialist

--- routes/specialistRoute.js
+++ routes/specialistRoute.js
@@ -11,67 +11,87 @@ router.get('/', (req, res) => {
 });
 
  
-  router.post('/', async(req, res) => { 
-    if (req.session) {
-    try{
-    console.log(req.body)
-    const specialistReg = new SpecialistReg(req.body);
-    
-    await specialistReg.save()
-    res.redirect('/specialistReg/specialistList')
-    }catch(err){
-      console.log(err);
-      res.send('oops something went wrong')
-    }
-    }
+  router.post('/', (req, res) => { 
+    const firstname = req.body.firstname;
+     const lastname = req.body.lastname;
+     const gender = req.body.gender;
+     const specialisation = req.body.specialisation;
+     const telephoneNo = req.body.telephoneNo;
+     const location = req.body.location;
+     const email = req.body.email;
+     
+     
+    //  console.log(name,email)
 
-    })
+   const specialistReg = new SpecialistReg({
+       firstname,
+       lastname,
+       specialisation,
+       gender,
+       telephoneNo,
+       location,
+       email
+
+       
+         
+
+   })
+   specialistReg.save(err=>{
+       if(err){
+           console.log("err is ")
+       }else{
+           res.redirect('/specialistReg/list')
+       }
+   })
+})
+
 
    
-    router.get('/specialistList', async (req, res) => {
-      try {
+    router.get('/list', (req, res) => {
+      SpecialistReg.find((err,docs)=>{
+        if(err) throw err;
        
-        let specialistRegDetails = await SpecialistReg.find();
-        if (req.query.lastname) {
-          specialistRegDetails = await SpecialistReg.find({ lastname: req.query.lastname })
-        }
-        res.render('specialistRegList', { users: specialistRegDetails, title: 'Specialist list' })
-    } catch (err) {
-        console.log(err)
-        res.send('Failed to retrive specialist details');
-    }
+        res.render('newlistforSpecialist',{
+            students : docs
+        })
+        
     })
+})
+
 
     
     // Obtaining data to be updated.
-    router.get('/update/:id', async (req, res) => {
-      try {
-          const updateSpe = await SpecialistReg.findOne({ _id: req.params.id })
-          res.render('updatespecialist', { user: updateSpe })
-      } catch (err) {
-          res.status(400).send("Unable to find item in the database");
-      }
+    router.get('/update/:id',(req, res) => {
+      SpecialistReg.findOneAndUpdate({_id: req.params.id},req.body,{new:true},(err,docs)=>{
+        if(err){
+            console.log("cant update")
+        }else{
+            res.render('speceditsform',{studentdata:docs})
+        }
     })
+})
 
 
 // Route to save the updated data.
-  router.post('/update', async (req, res) => {
-    try {
-        await SpecialistReg.findOneAndUpdate({_id:req.query.id}, req.body)
-        res.redirect('/specialistReg');
-    } catch (err) {
-        res.status(404).send("Unable to update item in the database");
-    }
+  router.post('/update/:id',(req,res)=>{
+    SpecialistReg.findByIdAndUpdate({_id:req.params.id},req.body,(err,docs)=>{
+      if(err){
+          console.log("err")
+      }else{
+          res.redirect('/specialistReg/list')
+      }
   })
+})
 
-
-router.post('/delete', async (req, res) => {
-  try {
-      await SpecialistReg.deleteOne({ _id: req.body.id })
-      res.redirect('back')
-  } catch (err) {
-      res.status(400).send("Unable to delete item in the database");
-  }
+router.get('/delete/:id',(req,res)=>{
+  SpecialistReg.findByIdAndDelete({_id:req.params.id},req.body,(err,docs)=>{
+       if(err){
+           console.log("Err is")
+       }else{
+           console.log("Delted")
+           res.redirect('/specialistReg/list')
+       }
+  })
 })
 
 
